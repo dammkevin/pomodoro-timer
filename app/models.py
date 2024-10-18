1d4fbe523aad155297dd102ca6497d68a0c82765
Kevin Dam <kevindam@Kevins-MacBook-Pro-3.local>
2024-10-18 16:59:46 -0400

    got rid of debugging print statements in models.py

--- app/models.py
+++ app/models.py
@@ -1,13 +1,9 @@
-print("Starting models.py...")  # This should print when the file is loaded
-
 from app import db
 from flask_login import UserMixin
 from datetime import datetime
 
-print("Imports successful")  # This should print if imports succeed
 
 class User(UserMixin, db.Model):
-    print("Defining User class")
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(20), unique=True, nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
@@ -17,7 +13,6 @@ class User(UserMixin, db.Model):
         return f"User('{self.username}', '{self.email}')"
 
 class Session(db.Model):
-    print("Defining Session class")
     id = db.Column(db.Integer, primary_key=True)
     start_time = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
     end_time = db.Column(db.DateTime)
@@ -26,5 +21,3 @@ class Session(db.Model):
 
     def __repr__(self):
         return f"Session('{self.id}', '{self.status}')"
-
-print("models.py has been successfully loaded")  # This should print if everything works
